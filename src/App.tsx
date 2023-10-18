06e03563665478552d13e28f1723523b4d6658ad
morpheuszein <morpheus.zein@irawan.id>
2023-10-19 06:16:34 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,78 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Key, useState, useRef, RefObject } from "react";
 import Uniqueid from "generate-unique-id";
-const id = Uniqueid({
-	length: 4,
-	useLetters: false,
-});
-console.log(id);
 function App() {
 	const inputref = useRef<HTMLInputElement>(null);
-	const [Todo, setTodo] = useState<Array<object>>([{}]);
+	const [Todo, setTodo] = useState<Array<any>>([]);
 
 	const handleAdd = (e: any) => {
 		const item = inputref.current?.value;
 		if (item === "") return;
-		setTodo((prevTodo) => {
-			return [...prevTodo, { id: id, nama: item, complete: false }];
+		setTodo((itemprev) => {
+			return [
+				...itemprev,
+				{
+					id: Uniqueid({
+						useLetters: false,
+						length: 4,
+					}),
+					nama: item,
+					complete: false,
+				},
+			];
 		});
 		if (inputref.current) inputref.current.value = "";
 	};
+	const handleToggle = (id: string) => {
+		const newTodos = Todo.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, complete: !todo.complete };
+			}
+			return todo;
+		});
+		setTodo(newTodos);
+	};
+	const clearTodoList = () => {
+		setTodo([]);
+	};
+
 	return (
 		<>
-			<Todoboard toditem={Todo} />
+			<h1>TodoList</h1>
+			<Todoboard toditem={Todo} toggletodo={handleToggle} />
 			<div>
 				<input type="text" ref={inputref} />
 				<button onClick={handleAdd}>Add Todo</button>
-				<h1>TodoList</h1>
+				<button onClick={clearTodoList}> Clear todo</button>
 			</div>
 		</>
 	);
 }
 
-function Todoboard({ toditem }: { toditem: Array<object> }) {
+function Todoboard({
+	toditem,
+	toggletodo,
+}: {
+	toditem: Array<object>;
+	toggletodo: any;
+}) {
 	return (
 		<>
 			<p className="text-red-500">
 				{toditem?.map((item: any) => {
-					return <Todoitem key={item.id} item={item} />;
+					return <Todoitem toggletodo={toggletodo} key={item.id} item={item} />;
 				})}
 			</p>
 		</>
 	);
 }
-function Todoitem({ item }: { item: any }) {
+function Todoitem({ item, toggletodo }: { item: any; toggletodo: any }) {
+	const handletoggle = () => {
+		toggletodo(item.id);
+	};
 	return (
 		<div className="hai">
-			<input type="checkbox" checked={item.complete} />
+			<input type="checkbox" checked={item.complete} onChange={handletoggle} />
 			<label htmlFor="hai">{item.nama}</label>
 		</div>
 	);
